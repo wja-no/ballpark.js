bd28d58d6822031c036598c67e926925050cb3fd
Carl Martin Rosenberg <cmrosenb@gmail.com>
2013-07-26 11:48:35 +0200

    Imported new testing functionality

--- app.js
+++ app.js
@@ -1,43 +1,110 @@
-(function(){
+(function(data, iterations, iframe, window, paragraph){
+
   if(window.performance === undefined){
-    alert("Browser not supported");
+    paragraph.childNodes[0].data  = "Unfortunately, your browser does not support the Navigation Timing API";
     return;
-  };
-
-  var result = {};
-  var iterations = 5;
-  var iframe = document.getElementsByTagName('iframe')[0];
-
-
-  for(var i = 0; i < TESTS.length; i++){
-    result[TESTS[i]] = new Array();
   }
+  function Runner(iframe, window, iterations){
 
-  var current;
-  var queue = new Array();
+    var result = {};
+    var current;
+    var queue;
+    var current_category;
 
-  for(var i = 0; i < iterations; i++){
-    for(var j = 0; j < TESTS.length; j++){
-      queue.push(TESTS[j]);
+    //We initialize a placeholder for the callee's callback-function.
+    //We use an empty function so that JavaScript treats the variable as 
+    //callable, although the actual function will be set later.
+
+    var finalize = function(){};
+
+    function testNext(){
+      current = queue.pop();
+      var currString = "./"+current_category+"/"+current+"/index.html";
+      console.log(currString);
+      iframe.src = currString;
+    }
+
+    function catchAndContinue(value){
+      result[current].push(value.data);
+      if(queue.length === 0){
+        finalize(result); 
+      }
+      else testNext();
+    }
+
+    window.addEventListener("message", catchAndContinue, false);
+
+    //return a function that sets the final callback to the one specified
+    //by the callee, builds the queue and result objects corresponding to the 
+    //callees testcase, and empties the queue using functions in the enclosing
+    //scope.  
+    return function(tests, category, callback){
+
+      finalize = callback;
+      current_category = category;
+
+      //Initialize a new queue with /iterations/ test-tokens per tests.
+      queue = new Array((tests.length*iterations));
+
+      for(var i = 0; i < tests.length; i++){
+
+        //Create an array for the results, and name this array after the test
+        result[tests[i]] = new Array();
+
+        //Add /iterations/ of the test to the queue
+        for(var j = 0; j < iterations; j++){
+          queue[iterations*i + j] = tests[i];
+        }
+      }
+
+      //Start the iteration
+      testNext();
     }
   }
 
-  function test(){
+  function process(data, runner, finish){
+
+    var results = {};
+    var queue = new Array();
+    var current;
+
+    for(var category in data){
+      results[category] = {};
+      queue.push(category);
+    }
+
+    function post(incoming_result){
+      results[current] = incoming_result;
+      if(queue[0] === undefined) finish(results);
+      else {
+        current = queue.pop();
+        runner(data[current], post)
+      }
+    }
+
     current = queue.pop();
-    iframe.src = current;
+    runner(data[current], current, post);
   }
 
-  function printResult(){
-    console.log(result[TESTS[0]])
+  function buildReport(array){
+    var naive_average = array.reduce(function(p, c, i, array){ return p+c })/array.length;
+    return naive_average;
   }
 
-  function testNext(value){
-    result[current].push(value.data);
-    if(queue[0] === undefined) printResult();
-    else test();
+  function present(result){
+    var raw_html = "<table>";
+    for(var name in result){
+      raw_html += "<tr><th>"+name+"</th></tr>";
+      var  category = result[name];
+      for(var testname in category){
+        raw_html += "<tr><td>"+testname+"</td>";
+        raw_html += "<td>"+buildReport(category[testname])+"</td></tr>";
+      }
+    }
+    raw_html += "</table>";
+    document.body.innerHTML = raw_html;
   }
 
-  window.addEventListener("message", testNext, false);
-  test();
+  process(data, Runner(iframe, window, iterations), present);
 
-}())
+} (TESTRUNNER.data, 10, document.getElementsByTagName('iframe')[0], window, document.getElementsByTagName('p')[0]));
--- tests.js
+++ tests.js
@@ -1,4 +1,3 @@
-var TESTS = new Array();
-TESTS.push('fonts/google-fonts/google-fonts-base64/index.html')
-TESTS.push('fonts/google-fonts/google-fonts-cdn/index.html')
-TESTS.push('fonts/google-fonts/google-fonts-local/index.html');
+var TESTRUNNER = {};
+TESTRUNNER.data = { fonts: ["from-cdn", "local", "local-base64"] };
+
