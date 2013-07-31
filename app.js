758e5c47f8a857883f0d155a1c1ffca1918dd0d7
Carl Martin Rosenberg <cmrosenb@gmail.com>
2013-07-31 11:14:08 +0200

    table updates too often

--- app.js
+++ app.js
@@ -26,7 +26,6 @@
 
     function checkParameters(){
       var timing = inner_window.performance.timing;
-      console.log("Calling checkParameters");
       if(timing.requestStart === 0) setTimeout(checkParameters, 10);
       else {
         var result = timing.loadEventStart - timing.requestStart;
@@ -40,21 +39,14 @@
 
       current_testno += 1;
       article.innerHTML = "<p>Running test <strong>"+current_testno+"</strong> of <strong>"+total_tests+"</strong>.</p>";
-      console.log("Calling testNext()");
       current = queue.pop();
       var currString = "./tests/"+current_category+"/"+current+"/index.html";
-      console.log("attempting to load "+currString);
       iframe.src = currString;
     }
 
     function catchAndContinue(message){
-      console.log("in catchAndContinue");
-      console.log(result);
       result[current] = message.data;
-      console.log("Catching result[current]");
-      console.log(result[current]);
       if(queue.length === 0){
-        console.log("finalizing");
         iframe.class = "hidden";
         finalize(result); 
       }
@@ -97,10 +89,6 @@
     }
 
     function buildResult(incoming){
-      console.log("buildResult called with");
-      console.log(incoming);
-      console.log("results-object looks like");
-      console.log(results);
       if(results[current] === undefined){
         results[current] = incoming;
         for(var subcategory in results[current]){
@@ -109,12 +97,6 @@
           results[current][subcategory] = array;
         }
       }else for(subcategory in incoming){
-        console.log("pushing result")
-        console.log(incoming[subcategory]);
-        console.log("to results[current]subcategory] where current is");
-        console.log(current);
-        console.log("And subcategory is");
-        console.log(subcategory);
         results[current][subcategory].push(incoming[subcategory]);
       }
     }
@@ -128,18 +110,17 @@
       }
     }
 
-    console.log(queue);
     current = queue.pop();
     runner(data[current].concat(), current, post);
   }
 
   function buildReport(array){
-    console.log(array);
     var naive_average = array.reduce(function(p, c, i, array){ return p+c })/array.length;
     return Math.round(naive_average);
   }
 
   function present(result){
+    console.log("in present");
     article.innerHTML = "<p>Alright! Testing completed.<p>Do take the results with a grain of salt. <p><button>One more time, please</button>";
     iframe.style.display = "none";
     var raw_html = "";
@@ -149,13 +130,15 @@
       for(var testname in category){
         var url = "<a href='./tests/"+name+"/"+testname+"/"+"'>"+testname+"</a>";
         raw_html += "<tr><td>"+url+"</td>";
-        console.log("Printing array of results from "+testname);
         raw_html += "<td>"+buildReport(category[testname])+"</td></tr>";
       }
     }
     table.innerHTML = raw_html;
     button = document.getElementsByTagName('button')[0];
-    button.onclick = engage;
+    button.onclick = function(){
+    iframe.style.display = "block";
+    process(data, Runner(iframe, window, iterations), present);
+    };
   }
 
   function engage(){
