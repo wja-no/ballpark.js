e167eb922ace225c16f27415ac4a491fa55d587e
Carl Martin Rosenberg <cmrosenb@gmail.com>
2013-08-01 14:41:17 +0200

    Added custom_font tests

--- tests.js
+++ tests.js
@@ -4,6 +4,7 @@ TESTRUNNER.data = {
   baseline: ["hello_world"],
 embedded_images: ["many_files", "single_base64"],
   resources: ["few", "many"],
+  custom_fonts: ["font_full", "font_subset"],
   images: ["avatars", "1136-pixel-image","2880-pixel-image"],
   //libraries: ["bootstrap-minified", "bootstrap-regular", "jquery-1-UI-local", "jquery-1-cdn", "jquery-1-local", "jquery-2-cdn", "jquery-2-local"],
   //composite: ["bestcase","worstcase"],
