eeb34dc2bec71e0e87ed34ae749f37c63f818a11
Carl Martin Rosenberg <cmrosenb@gmail.com>
2013-08-07 12:14:07 +0200

    Wrote more information about tests. Testrunner now filters extreme values.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 (function(data, iterations, iframe, window, article, button, table){
 
-  if(window.performance === undefined){
-    article.innerHTML = "Unfortunately, your browser does not support the Navigation Timing API";
+  if(window.performance === undefined) {
+    article.innerHTML = "<p>Unfortunately, your browser does not support the <a href='http://caniuse.com/#search=navigation%20timing%20api'>Navigation Timing API</a></p>";
     return;
   }
 
-  function createBindedRunner(iframe, window){
+  function createBindedRunner(iframe, window) {
 
     var result, number_of_tests, counter, current_test, queue, finalize;
 
@@ -14,18 +14,18 @@
     var inner_window = iframe.contentWindow;
     inner_window.domain = document.domain; 
 
-    function reportTestNumber(){
+    function reportTestNumber() {
       counter += 1;
       article.innerHTML = "<p>Running test <strong>"+counter+"</strong> of <strong>"+number_of_tests+"</strong>.</p>";
     }
 
-    function testNext(){
+    function testNext() {
       reportTestNumber();
       current_test = queue.shift();
       iframe.src = "./tests/"+current_test[0]+"/"+current_test[1]+"/index.html";
     }
 
-    function initializeIfEmpty(){
+    function initializeIfEmpty() {
       if(result[current_test[0]] === undefined)
         result[current_test[0]] = {};
 
@@ -33,12 +33,12 @@
         result[current_test[0]][current_test[1]] = [];
     }
 
-    function iterate(){
+    function iterate() {
       if(queue.length !== 0) testNext();
       else finalize(result);
     }
 
-    function catchMessage(message){
+    function catchMessage(message) {
       initializeIfEmpty();
       result[current_test[0]][current_test[1]].push(message.data);
       iterate();
@@ -46,24 +46,24 @@
 
     window.addEventListener("message", catchMessage, false);
 
-    function bindPoster(inner_window, callback){
-      function postIfSet(){
+    function bindPoster(inner_window, callback) {
+      function postIfSet() {
         var timing = inner_window.performance.timing;
-        if(timing.requestStart === 0) setTimeout(postIfSet, 10);
+        if(timing.loadEventEnd === 0) setTimeout(postIfSet, 10);
         else callback(inner_window)
       }
       return postIfSet;
     }
 
-    function postStandardResult(inner_window){
+    function postStandardResult(inner_window) {
       var timing = inner_window.performance.timing;
-      var result = timing.loadEventStart - timing.requestStart;
+      var result = timing.loadEventEnd - timing.requestStart;
       inner_window.parent.postMessage(result, "*");
     }
 
     iframe.onload = bindPoster(inner_window, postStandardResult);
 
-    return function(tests, callback){
+    return function(tests, callback) {
       number_of_tests = tests.length;
       queue = tests;
       counter = 0;
@@ -73,7 +73,7 @@
     }
   }
 
-  function buildQueue(data){
+  function buildQueue(data) {
     var queue = [];
     for(var i = 0; i < iterations; i++){
       for(var category in data){
@@ -83,12 +83,37 @@
     return queue;
   }
 
-  function findAverage(array){
-    var naive_average = array.reduce(function(p, c, i, array){ return p+c })/array.length;
+  function findMaxMin(array) {
+    var max = -Number.MAX_VALUE;
+    var min = Number.MAX_VALUE;
+    array.forEach(function(element) {
+      if(element >= max) max = element;
+      if(element <= min) min = element;
+    });
+    return [min, max];
+  }
+
+  function filterExtremes(smallest, largest) {
+    var removals = 0;
+    return function(value, index, array) {
+      if(value !== smallest && value !== largest) return true;
+      else {
+        removals += 1;
+        if(removals <= 2) return false;
+        else return true;
+      }
+    }
+  }
+
+  function findAverage(array) {
+    console.log(array);
+    var trimmed_array = array.filter(filterExtremes.apply(null, findMaxMin(array)));
+    console.log(trimmed_array);
+    var naive_average = trimmed_array.reduce(function(p, c, i, trimmed_array){ return p+c })/trimmed_array.length;
     return Math.round(naive_average);
   }
 
-  function createTable(result){
+  function createTable(result) {
     iframe.style.display = "none";
     var raw_html = "";
     for(var name in result){
@@ -103,7 +128,7 @@
     table.innerHTML = raw_html;
   }
 
-  function present(result){
+  function present(result) {
     article.firstChild.textContent = "Alright! Testing completed.";
     var second_paragraph = document.createElement('p');
     second_paragraph.textContent = "Do take the results with a grain of salt.";
@@ -115,7 +140,7 @@
 
   var runner = createBindedRunner(iframe, window);
 
-  function engage(){
+  function engage() {
     table.innerHTML = "";
     iframe.style.display = "block";
     runner(buildQueue(data), present);
--- tests.js
+++ tests.js
@@ -2,11 +2,11 @@ var TESTRUNNER = {};
 
 TESTRUNNER.data = {
   baseline: ["hello_world"],
-embedded_images: ["many_files", "single_base64"],
+  embedded_images: ["many_files", "single_base64"],
   resources: ["few", "many"],
   custom_font: ["font_full", "font_subset"],
   images: ["avatars", "1136-pixel-image","2880-pixel-image"],
   //libraries: ["bootstrap-minified", "bootstrap-regular", "jquery-1-UI-local", "jquery-1-cdn", "jquery-1-local", "jquery-2-cdn", "jquery-2-local"],
-  //composite: ["bestcase","worstcase"],
+  composite: ["bestcase","worstcase"],
   external_embeds: ["facebook-likebox"]
 }
