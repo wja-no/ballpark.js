2d5435b74e4304dc54a6955ccd2acfe266a98a3b
Carl Martin Rosenberg <cmrosenb@gmail.com>
2013-08-07 14:08:20 +0200

    Removed CDN-related tests

--- tests.js
+++ tests.js
@@ -1,12 +1,12 @@
 var TESTRUNNER = {};
 
 TESTRUNNER.data = {
-  baseline: ["hello_world"],
-  embedded_images: ["many_files", "single_base64"],
-  resources: ["few", "many"],
+   baseline: ["hello_world"],
+   embedded_images: ["many_files", "single_base64"],
+   resources: ["few", "many"],
+   fonts: ["local", "local-base64"],
   custom_font: ["font_full", "font_subset"],
   images: ["avatars", "1136-pixel-image","2880-pixel-image"],
-  //libraries: ["bootstrap-minified", "bootstrap-regular", "jquery-1-UI-local", "jquery-1-cdn", "jquery-1-local", "jquery-2-cdn", "jquery-2-local"],
-  composite: ["bestcase","worstcase"],
-  external_embeds: ["facebook-likebox"]
+  libraries: [, "jquery-1-UI-local", "jquery-1-local", "jquery-2-local"],
+   composite: ["bestcase"]
 }
