0d2db0cd0fcc5b55c672e69b98f7f5d640b38f74
Carl Martin Rosenberg <cmrosenb@gmail.com>
2013-08-09 12:55:45 +0200

    Removing the local test as it is not representative for actual cost of font loading

--- tests/fonts/local/measurements/values.py
+++
@@ -1,7 +0,0 @@
-sorted_vals = [51, 54, 54, 54, 56, 56, 56, 58, 62, 67]
-
-arithmetic_mean = sum(sorted_vals) / 10.
-median = (sorted_vals[4] + sorted_vals[5]) / 2.
-
-print 'arithmetic mean: %f' %arithmetic_mean
-print 'median : %f' %median
