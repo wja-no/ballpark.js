1c434e91fad7480a1b4e3752a2c53cda4b7c8aae
Carl Martin Rosenberg <cmrosenb@gmail.com>
2013-08-14 16:34:06 +0200

    cleaning up

--- tests/fonts/local-base64/measurements/results.py
+++
@@ -1,5 +0,0 @@
-ten_mid_values_sorted = [64, 67, 67 , 70, 71, 71 , 73, 73, 75 , 78]
-arithmetic_mean = sum(ten_mid_values_sorted) / 10.
-median = (ten_mid_values_sorted[4] + ten_mid_values_sorted[5]) / 2.
-print 'arithmetic mean: %f' %arithmetic_mean 
-print 'median: %f' %median
