ba66e63ea7f5863cc35ae95dbd9a6e3d7caafcd1
Carl Martin Rosenberg <cmrosenb@gmail.com>
2013-08-15 13:19:40 +0200

    Fixed typo in tests

--- tests.js
+++ tests.js
@@ -5,7 +5,7 @@ TESTRUNNER.data = {
    embedded_images: ["many_files", "single_base64"],
    resources: ["few", "many"],
    fonts: ["local-base64"],
-  custom_font: ["font_full", "font_subset"],
+  custom_font: ["full", "subset"],
   images: ["avatars", "1136-pixel-image","2880-pixel-image"],
   libraries: [, "jquery-1-UI-local", "jquery-1-local", "jquery-2-local"]
 }
